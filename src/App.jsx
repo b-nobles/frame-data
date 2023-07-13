7f7e6783511c4d0962292e83abc75948caaec300
Brandon Nobles <bjnobles10@gmail.com>
2023-07-12 20:08:12 -0400

    Update image and text UI

--- src/App.jsx
+++ src/App.jsx
@@ -75,6 +75,26 @@ export default function App() {
       onblock: -12,
       image: "/src/assets/ryu/crHK.png",
     },
+    targets: {
+      "sHP>HK": {
+        name: "High Double Strike",
+        startup: 19,
+        onblock: -8,
+        image: "/src/assets/ryu/sHPHK.png"
+      },
+      "sMP>LK": {
+        name: "Fuwa Double Strike",
+        startup: 11,
+        onblock: -7,
+        image: "/src/assets/ryu/sMPLK.png"
+      },
+      "sMP>LK>HK": {
+        name: "Fuwa Triple Strike",
+        startup: 27,
+        onblock: -8,
+        image: "/src/assets/ryu/sMPLKHK.png"
+      }
+    },
     commands: {
       fMP: {
       name: "Collarbone Breaker",
@@ -184,7 +204,27 @@ export default function App() {
       onblock: -10,
       image: "/src/assets/ken/crHK.png",
     },
-    profile: "/src/assets/ken/profile.png"
+    profile: "/src/assets/ken/profile.png",
+    targets: {
+      "sMP>HP": {
+        name: "Chin Buster",
+        startup: 16,
+        onblock: -14,
+        image: "/src/assets/ken/sMPHP.png"
+      },
+      "sMK>MK": {
+        name: "Triple Flash Kicks 1",
+        startup: 19,
+        onblock: -12,
+        image: "/src/assets/ken/sMKMK.png"
+      },
+      "sMK>MK>HK": {
+        name: "Triple Flash Kicks 2",
+        startup: 32,
+        onblock: -11,
+        image: "/src/assets/ken/sMKMKHK.png"
+      }
+    }
   };
 
   const [newMove, setNewMove] = useState("sLP");
@@ -197,11 +237,15 @@ export default function App() {
   let pressedButton = "LP"
   const [buttonPrefix, setButtonPrefix] = useState("s")
   const [command, setNewCommand] = useState("fMP")
+  const [target, setNewTarget] = useState("sHP>HK")
   const [attackingState, setNewAttackingState] = useState("normals")
+  const [menuState, setNewMenuState] = useState("target combos")
+  const [newImage, setNewImage] = useState('/src/assets/ryu/sLP.png')
 
   //handle the radio option for direction for an input
   function directionSelect(e) {
     setButtonPrefix(e.target.value);
+    setNewImage(attackChar[e.target.value + activeButton].image)
   }
 
   //button state, pos 1 is for radio buttons, pos 2 is for attack buttons
@@ -227,17 +271,8 @@ export default function App() {
     setActiveButton(e.target.innerText)
     pressedButton = e.target.innerText
     setNewMove(buttonPrefix + pressedButton)
+    setNewImage(attackChar[buttonPrefix + pressedButton].image)
     setNewText("")
-  }
-
-  //proper dynamic attack image loader to handle different states
-  function imageLoader(prop) {
-    if (attackingState === "normals") {
-      return attackChar[newMove][prop]
-    } else if (attackingState === "command normals") {
-      return attackChar.commands[command][prop]
-    }
-
   }
 
   //create the buttons and give them the correct properties
@@ -249,12 +284,25 @@ export default function App() {
 
   //formula used to compare a move's frame data against the blocking character's entire moveset to find appropriate moves
   function frameChecker(attack, blocker) {
+    //Grab the necessary moves
     const blockerMoves = Object.keys(blocker);
+    let blockerCommands = ""
+    if (blocker.commands) {
+      blockerCommands = Object.keys(blocker.commands)
+    }
     let attackerMove = ''
     if (attackingState === 'normals') {
       attackerMove = attackChar[attack].onblock
-    } else if (attackingState === 'command normals') {
-      attackerMove = attackChar.commands[attack].onblock
+    } else if (attackingState + menuState === 'uniquecommand normals') {
+      attackerMove = attackChar.commands[command].onblock
+    } else if (attackingState + menuState === 'uniquetarget combos') {
+      const targetCombos = Object.keys(attackChar.targets)
+      if (targetCombos.includes(target)) {
+        attackerMove = attackChar.targets[target].onblock
+      } else {
+        attackerMove = attackChar.targets[targetCombos[0]].onblock
+      }
+      
     }
     const moveList = [];
     blockerMoves.forEach((move) => {
@@ -262,17 +310,19 @@ export default function App() {
         moveList.push(`${blocker[move].name} `);
       }
     });
+    if (blockerCommands) {
+      blockerCommands.forEach((move) => {
+        if (blocker.commands[move].startup <= attackerMove * -1) {
+          moveList.push(`${blocker.commands[move].name} (${move})`)
+        }
+      })
+    }
     if (moveList.length === 0) {
       setNewText("Cannot be punished")
     } else {
       setNewText(moveList.join("/ "));
     }
     
-  }
-
-  //function used to check for command normals
-  function commandNormal(e) {
-    setNewCommand(e.target.value)
   }
 
   //stop the form from refreshing the page, execute frameChecker function
@@ -288,6 +338,21 @@ export default function App() {
         return <option key={current.name}>{current.name}</option>;
       }
     });
+  }
+
+  //Check if a character has target combos or command normals
+  function attackMenu(attacker) {
+    const list = Object.keys(attacker);
+    const results = []
+    if (list.includes('targets')) {
+      results.push('Target Combos')
+    }
+    if (list.includes('commands')) {
+      results.push('Command Normals')
+    }
+    return results.map((name) => {
+      return <option key={name}>{name}</option>
+    })
   }
 
   //map moves into a dropdown
@@ -312,18 +377,93 @@ export default function App() {
     }
   }
 
+
   //change the attack character's moves when new character is selected
   function handleAttackerChange(e) {
-    //find the appropriate character using their name
-    lookUpCharacter(setAttackChar, e.target.value)
-    if (!attackChar.hasOwnProperty(buttonPrefix + activeButton)) {
-      setNewMove("sLP")
+    let currentAttacker = {};
+    //find the current character and assign their target combo to be first
+    for (const check in characterList) {
+      if (characterList[check].name === e.target.value) {
+        currentAttacker = characterList[check]
+      }
     }
+    if (attackingState === 'normals') {
+      setNewImage(currentAttacker[newMove].image);
+    } else if (attackingState === 'unique') {
+      setNewMenuState('target combos')
+      const targetCombos = Object.keys(currentAttacker.targets)
+      setNewImage(currentAttacker.targets[targetCombos[0]].image)
+    }
+      //find the appropriate character using their name
+      lookUpCharacter(setAttackChar, e.target.value)
+      console.log(target + ' ' + command)
+  }
+
+  //update menu state to change the corresponding drop down
+  function handleMenuChange(e) {
+    const menuText = e.target.value.toLowerCase();
+    console.log(menuText)
+    setNewMenuState(menuText)
+    setNewText("")
+    if (menuText === 'command normals') {
+      setNewImage(attackChar.commands[command].image)
+    } else if (menuText === 'target combos') {
+      const targetCombos = Object.keys(attackChar.targets)
+      if (!targetCombos.includes(target)) {
+        setNewImage(attackChar.targets[targetCombos[0]].image)
+      } else {
+        setNewImage(attackChar.targets[target].image)
+      }
+    }
+  }
+
+  //image resetting function to account for faulty target combos
+  function imageReset() {
+    const targetCombos = Object.keys(attackChar.targets)
+    if (!targetCombos.includes(target)) {
+      setNewTarget(targetCombos[0])
+      setNewImage(attackChar.targets[targetCombos[0]].image)
+    } else {
+      setNewImage(attackChar.targets[target].image)
+    }
+      setNewMenuState('target combos')
+      setNewText("");
   }
 
   //change the blocker and update blocker movelist
   function handleBlockerChange(e) {
     lookUpCharacter(setBlockChar, e.target.value)
+  }
+
+  //handle unique move changes and update appropriately
+  function handleUniqueChange(e) {
+    const listItem = e.target.value
+    setNewText("")
+    if (attackingState + menuState === 'uniquecommand normals') {
+      setNewCommand(listItem)
+      setNewImage(attackChar.commands[listItem].image)
+    } else if (attackingState + menuState === 'uniquetarget combos') {
+      setNewTarget(listItem)
+      setNewImage(attackChar.targets[listItem].image)
+    }
+  }
+
+  //grab appropriate text information
+  function textLoad(prop) {
+    if (attackingState === 'normals') {
+      return attackChar[newMove][prop];
+    } else if (attackingState + menuState === 'uniquetarget combos') {
+      const targetCombos = Object.keys(attackChar.targets)
+      if (targetCombos.includes(target)) {
+        return attackChar.targets[target][prop]
+      } else {
+        return attackChar.targets[targetCombos[0]][prop]
+      }
+      
+    } else if (attackingState + menuState === 'uniquecommand normals') {
+      //IF THERE IS AN ISSUE WITH COMMAND NORMALS NOT MATCHING UP COME BACK HERE
+      return attackChar.commands[command][prop]
+    }
   }
 
   return (
@@ -332,20 +472,16 @@ export default function App() {
       <div className="attack float">
       <h2>Attacker: {attackChar.name}</h2>
       <div className="attack-box">
-        <img
-          src={`${imageLoader('image')}`}
-          alt={`${attackChar.name} using ${imageLoader('name')}`}
-          className="attacking-character"
-        />
+        <img src={`${newImage}`} alt="" className="attacking-character"/>
       </div>
       <h3>
-        {imageLoader('name')} is {imageLoader('onblock')} on block
+      {`${textLoad('name')} is ${textLoad('onblock')} on block`}
       </h3>
       <label htmlFor="attackselect">Attacking Character: </label>
       <select
         name="attackselect"
         id="attackselect"
-        onChange={handleAttackerChange}
+        onChange={(handleAttackerChange)}
       >
         {characterFill()}
       </select>
@@ -354,12 +490,17 @@ export default function App() {
       <label htmlFor="standing">Standing</label>
       <input type="radio" id="crouching" name="direction" value="cr" onClick={directionSelect} onChange={() => (activeState("cr", 1), switchState('cr'))}/>
       <label htmlFor="crouching">Crouching</label>
-      <input type="radio" id="command-moves" name="direction" value='command' onChange={() => {setNewAttackingState('command normals')}}/>
-      <label htmlFor="command-moves">Command Normals</label>
+      <input type="radio" id="unique" name="direction" value='command' onChange={() => (setNewAttackingState('unique'), imageReset())}/>
+      <label htmlFor="unique">Unique</label>
       <br />
-      <label htmlFor="commands" hidden={(attackingState !== 'command normals')}>Command Normals: </label>
-      <select name="commands" id="commands" hidden={(attackingState !== 'command normals')} onChange={commandNormal}>
+      <select name="move-options" id="move-options" hidden={(attackingState !== 'unique')} onChange={handleMenuChange}>
+        {attackMenu(attackChar)}
+      </select>
+      <select name="commands" id="commands" hidden={(attackingState + menuState !== 'uniquecommand normals')} onChange={handleUniqueChange}>
         {attacksFill(attackChar.commands)}
+      </select>
+      <select name="targets" id="targets" hidden={(attackingState + menuState !== 'uniquetarget combos')} onChange={handleUniqueChange}>
+        {attacksFill(attackChar.targets)}
       </select>
       <div className="button-layout" hidden={(attackingState !== 'normals')}>{createButtons(attackButtons)}</div>
       </div>
